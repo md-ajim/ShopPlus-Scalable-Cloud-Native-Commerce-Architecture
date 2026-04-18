f325a379fa2815ac0bb527f1dcdbf40983493f1d
md-ajim <mdajim2669@gmail.com>
2026-04-18 17:35:46 +0600

    Remove unused portfolio URL inclusion
    The `portfolio/` URL inclusion has been removed from `urls.py` as it is no longer needed. This simplifies the URL configuration and removes unnecessary references to outdated or unused code.

--- backend/settings/urls.py
+++ backend/settings/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('nextCart.urls')),
     path('api/', include('authUser.urls')),
-    path('portfolio/', include('portfolio.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
